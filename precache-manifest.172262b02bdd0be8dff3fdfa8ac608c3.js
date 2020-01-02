5e73b018ca8dfa73a774a170ff1e320970742323
abhishek305 <abhishekshaji1994@gmail.com>
2020-01-02 22:40:26 +0530

    Updates

---
+++ precache-manifest.172262b02bdd0be8dff3fdfa8ac608c3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f8a4013479be60ba11a2dbd5af8918",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "48032c19999175b0ded1",
+    "url": "/react-app/static/css/2.debc53f4.chunk.css"
+  },
+  {
+    "revision": "5729ae7872a457c42bc1",
+    "url": "/react-app/static/css/main.bbea0f87.chunk.css"
+  },
+  {
+    "revision": "48032c19999175b0ded1",
+    "url": "/react-app/static/js/2.5c54fa9b.chunk.js"
+  },
+  {
+    "revision": "fcea170a450872dfe1aa41755b8cf1a3",
+    "url": "/react-app/static/js/2.5c54fa9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5729ae7872a457c42bc1",
+    "url": "/react-app/static/js/main.017757bd.chunk.js"
+  },
+  {
+    "revision": "088c542121d309054b52",
+    "url": "/react-app/static/js/runtime-main.037dc047.js"
+  },
+  {
+    "revision": "005c78c78009ef0743df0c256d11dbd8",
+    "url": "/react-app/static/media/banner-main.005c78c7.jpg"
+  },
+  {
+    "revision": "309da2380560d22a2e4e4e4c12e47ba8",
+    "url": "/react-app/static/media/banner-main.309da238.png"
+  },
+  {
+    "revision": "a247f4358b253516c32943603c4c5970",
+    "url": "/react-app/static/media/fontawesome-webfont.a247f435.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "20b4568f221f9a1295dbd11438cf6d2b",
+    "url": "/react-app/static/media/product-bg.20b4568f.jpg"
+  },
+  {
+    "revision": "654759a8e126c089c412a56e91356d14",
+    "url": "/react-app/static/media/product-bg.654759a8.png"
+  },
+  {
+    "revision": "0b0bbfa2474a7b461d55651f2a164979",
+    "url": "/react-app/static/media/quote-bg.0b0bbfa2.png"
+  },
+  {
+    "revision": "05834b1f74e5280406e475e339b0cd9b",
+    "url": "/react-app/static/media/untitled-font-4.05834b1f.svg"
+  },
+  {
+    "revision": "0596cfb0d5307c12d2555e1766e6f185",
+    "url": "/react-app/static/media/untitled-font-4.0596cfb0.woff"
+  },
+  {
+    "revision": "1801c8f2c4f0c4168ffe509775670857",
+    "url": "/react-app/static/media/untitled-font-4.1801c8f2.eot"
+  },
+  {
+    "revision": "b5ee7621a9e584bdccddbc384ab67dc0",
+    "url": "/react-app/static/media/untitled-font-4.b5ee7621.ttf"
+  },
+  {
+    "revision": "619fed819296a9dd42fb3b61fa672fce",
+    "url": "/react-app/static/media/video-bg.619fed81.png"
+  }
+]);
